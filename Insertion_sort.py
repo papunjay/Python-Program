f987e91df8d454e5cdf93070b9dcc197adcdf339
papunjay_kumar <kumarpapunjay@gmail.com>
2020-03-05 19:25:37 +0530

    [papunjay].Add write the sorting Algorithms

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,8 +1,25 @@
 from array import *
-list_data=[]
-Number=int(input("Enter the Number data"))
-for i in  range(Number):
-    x=int(input())
-    list_data.append(x)
-print("Your Entered Data")
-print(list_data)
+arr=array('i',[])
+'''def print_data(arr):
+    for i in range(0,len(arr)):
+        print(arr[i])
+'''
+def insertion_sort(arr):
+    for j in range(1,(len(arr))):
+        key=arr[j]
+        i=j-1
+        while(i>=0 and arr[i]>key):
+            arr[i+1]=arr[i]
+            i=i-1
+
+        arr[i+1]=key
+    return arr
+ #main()   
+print("Enter the size of Array")
+Number=int(input())
+for i in range(Number):
+    arr.append(int(input()))
+
+print(arr)
+sort_data=insertion_sort(arr)
+print(arr)
