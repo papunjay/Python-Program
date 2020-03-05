c70b75aa25cbddafd7fa5a8e1f7b9495c05f6182
papunjay_kumar <kumarpapunjay@gmail.com>
2020-03-05 18:23:36 +0530

    [papunjay].write the code to take the data for sorting

---
+++ Insertion_sort.py
@@ -0,0 +1,8 @@
+from array import *
+list_data=[]
+Number=int(input("Enter the Number data"))
+for i in  range(Number):
+    x=int(input())
+    list_data.append(x)
+print("Your Entered Data")
+print(list_data)
