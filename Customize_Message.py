0eb3c6b3ff18b58f4bf83712974a22bb22d0dc29
papunjay <30788782+papunjay@users.noreply.github.com>
2020-03-06 14:31:12 +0530

    Delete Customize_Message.py

--- Customize_Message.py
+++
@@ -1,6 +0,0 @@
-first_name=str(input("Enter the first name"))
-last_name=str(input("Enter the last name"))
-mob_no=int(input("Enter the mobile number"))
-date=[]
-date=input("Enter the date")
-print("Hello {} We have your full name as {} {} in our system. your contact number is {} Please,let us know in case of any clarification Thank you BridgeLabz {}. ".format(first_name,first_name,last_name,mob_no,date))
