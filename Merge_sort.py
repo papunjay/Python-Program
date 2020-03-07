5a4ee4b7fc437f0914723f5d1c6e36885a016e71
papunjay <kumarpapunjay@gmail.com>
2020-03-07 10:38:08 +0530

    [papunjay].Add code to take value

---
+++ Merge_sort.py
@@ -0,0 +1,6 @@
+from  array import *
+arr=[]
+Number=int(input("Enter the Size of Array\n"))
+for arr_index in range(Number):
+    arr.append(int(input()))
+print("Your Entered data is \n {}".format(arr))
