2cbad0c8e32e5cf01ee642623885dd506dab8461
papunjay <kumarpapunjay@gmail.com>
2020-03-07 15:36:02 +0530

    [papunjay].solve the core dumped

--- Merge_sort.py
+++ Merge_sort.py
@@ -1,45 +1,35 @@
+import sys
+sys.setrecursionlimit(10**6) 
+def merge_sort(arr):
+    if len(arr)>1:
+        mid_index=len(arr)//2
+        left_arr=arr[:mid_index]
+        right_arr=arr[mid_index:]
 
-def merge(arr,start_index,mid_index,last_index):
-    #left_index=0
-   # right_index=0
-    no_of_left_child = mid_index-start_index+1
-    no_of_right_child = last_index-mid_index
-    #left_arr = [no_of_left_child]
-    #right_arr = [no_of_right_child]
-    left_arr=[0]*(no_of_left_child)
-    right_arr=[0]*(no_of_right_child)
-    for left_index in range(0,no_of_left_child):
-        left_arr.append(arr[start_index+left_index])
-    for right_index in range(0,no_of_right_child):
-        right_arr.append(arr[mid_index+right_index+start_index])
+        merge_sort(left_arr)
+        merge_sort(right_arr)
 
-    left_index=0 
-    right_index=0
-    while(left_index<no_of_left_child and right_index < no_of_right_child):
-        if left_arr[left_index]<= right_arr[right_index]:
-            arr.append(left_arr[left_index])
+        left_index=right_index=arr_index=0
+        while(left_index<len(left_arr) and right_index < len(right_arr)):
+            if left_arr[left_index]< right_arr[right_index]:
+                arr[arr_index]=left_arr[left_index]
+                left_index+=1
+            else:
+                arr[arr_index]=right_arr[right_index]
+                right_index+=1
+            arr_index+=1
+        while(left_index < len(left_arr)):
+            arr[arr_index]=left_arr[left_index]
             left_index+=1
-        else:
-            arr.append(right_arr[right_index])
+        while(right_index<len(right_arr)):
+            arr[arr_index]=(right_arr[right_index]) 
             right_index+=1
-    while(left_index < no_of_left_child):
-        arr.append(left_arr[left_index])
-        left_index+=1
-    while(right_index<no_of_right_child):
-        arr.append(right_arr[right_index]) 
-        right_index+=1
-def merge_sort(arr,start_index,last_index):
-    if last_index > start_index:
-        mid_index=(start_index+(last_index-start_index))//2
-        merge_sort(arr,start_index,mid_index)
-        merge_sort(arr,mid_index+1,start_index)
-        merge(arr,start_index,mid_index,last_index)
 
 arr=[]
 Number=int(input("Enter the Size of Array\n"))
 for arr_index in range(Number):
     arr.append(int(input()))
-print("Your Entered data is \n 5{}".format(arr))
-merge_sort(arr,0,Number-1)
+print("Your Entered data is \n {}".format(arr))
+merge_sort(arr)
 print(arr)
 
