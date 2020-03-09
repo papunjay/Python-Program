b4d2537a6edd5be618d3a824b64668139183556c
papunjay <kumarpapunjay@gmail.com>
2020-03-09 10:35:51 +0530

    remove the extra output

--- find_your_number.py
+++ find_your_number.py
@@ -1,7 +1,20 @@
 import sys
 from sys import argv
-def Binary(Number,start,end):
-    mid=start+(end-start)//2
-    print(mid)
+def Binary(low,high):
+    print("your data low={} and high={} is less then {}".format(low,high,high))
+    ans=str(input()) 
+    if(high-low)==1:
+        print(low)
+    else:
+        if(ans=='yes'):
+            mid=(low+(high-low)//2)
+            return Binary(low,mid)
+        else:
+            low=high
+            high=low*2
+            mid2=(low+(high-low)//2)
+            return Binary(low,mid2)
+
 Number=int(sys.argv[1])
-Binary(Number,0,Number)
+result=Binary(0,Number)
+print(result)
