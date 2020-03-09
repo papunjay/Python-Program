19781659f9b9174a8bf080d532b5347a81f992a8
papunjay <kumarpapunjay@gmail.com>
2020-03-09 13:27:28 +0530

    [papunjay].create class linklist ,method Insert data

--- Unordered_LinkList.py
+++ Unordered_LinkList.py
@@ -1,6 +1,43 @@
-f=open("new.txt","w")
-f.write("papunjay kumar hii no r u maid")
-f.close()
-f1=open("new.txt","r")
-print(f1.read())
+'''
+'''
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
 
+class LinkList:
+    def __init__(self):
+        self.head=None
+    
+    def Insert_Last(self,value):
+        newNode=node(value)
+        if(self.head==None):
+            self.head=newNode
+        else:
+            temp=self.head
+            while temp.next!=None:
+                temp=temp.next
+            temp.next=newNode
+    def display(self):
+        if(self.head==None):
+            print("list is empaty")
+        else:
+            temp=self.head
+            while temp!=None:
+                print(temp.data)
+                temp=temp.next
+'''
+    def Delete_node(self):
+        if(self.data==None):
+            print("List id empaty")
+        else:
+            temp=self.head
+            while temp.next.data!==user_data:
+                temp=temp.next
+            '''
+my_list=LinkList()
+my_list.Insert_Last(5)
+my_list.Insert_Last(8)
+my_list.Insert_Last(10)
+my_list.Insert_Last(16)
+my_list.display()
