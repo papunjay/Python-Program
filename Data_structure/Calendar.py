4153f66e791eae283f4578af69bd3f6f5ff944ca
papunjay <kumarpapunjay@gmail.com>
2020-03-13 12:50:33 +0530

    [papunjay]Add list of day

--- Data_structure/Calendar.py
+++ Data_structure/Calendar.py
@@ -5,3 +5,4 @@ months=["January", "February", "March",
             "April", "May", "June",
             "July", "August", "September",
             "October", "November", "December"]
+days=[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
