763f757b6add8bfdfe705dee30e6b34b0103c914
papunjay <kumarpapunjay@gmail.com>
2020-03-13 12:51:30 +0530

    [papunjay]Add isLeapyear

--- Data_structure/Calendar.py
+++ Data_structure/Calendar.py
@@ -1,3 +1,14 @@
+def isLeapyear(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
 year=int(input())
 month=int(input())
 
