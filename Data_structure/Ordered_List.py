718a66d9ba7982c4c2817cde7511ca9e604cb96c
papunjay <kumarpapunjay@gmail.com>
2020-03-13 11:16:48 +0530

    [papunjay].Add sort the data and append in the list

--- Data_structure/Ordered_List.py
+++ Data_structure/Ordered_List.py
@@ -3,3 +3,8 @@ f=open("file.txt","r")
 for word in f.read().split():
     list.append(int(word))
 print("your entered data\n{}".format(list))
+
+list=sorted(list)
+my_list=LinkList()
+for data in list:
+    my_list.Insert_Last(data)
