c992ad995ce4ab6b7ff1f125509fcc8066eb2721
papunjay <kumarpapunjay@gmail.com>
2020-03-13 12:44:50 +0530

    [papunjay].remove error

--- Data_structure/Ordered_List.py
+++ Data_structure/Ordered_List.py
@@ -5,7 +5,7 @@ class node:
 
 class LinkList:
     def __init__(self):
-        self.head=None  
+        self.head=None
 
     def Insert_Last(self,value):
         newNode=node(value)
@@ -15,8 +15,7 @@ class LinkList:
             temp=self.head
             while temp.next!=None:
                 temp=temp.next
-            temp.next=newNode 
-
+            temp.next=newNode
     def Insert_at_position(self,search_data):
         newNode=node(search_data)
         temp=self.head
@@ -37,6 +36,14 @@ class LinkList:
                 return
           
             temp=temp.next
+    def display(self):
+        if self.head==None:
+            print("Link list is empty")
+        else:
+            temp=self.head
+            while temp!=None:
+                print(temp.data)
+                temp=temp.next
 
     def search_input(self,search_data):
         if(self.head==None):
@@ -49,13 +56,13 @@ class LinkList:
                 temp=temp.next
                 if(temp==None):
                     return 0
-    def delete_search_data(self,search_data):  
+
+    def delete_search_data(self,search_data):
         temp=self.head
         if temp==search_data:
             self.head=temp.next
             temp=None
-            return
-
+            return 
         else:
             temp=self.head
             while temp!=None:
@@ -64,15 +71,7 @@ class LinkList:
                 prev=temp
                 temp=temp.next
             prev.next=temp.next
-    def display(self):
-        if self.head==None:
-            print("Link list is empty")
-        else:
-            temp=self.head
-            while temp!=None:
-                print(temp.data)
-                temp=temp.next        
-    
+
     def LinkList_to_List(self):
         list=[]
         temp=self.head
@@ -85,8 +84,8 @@ f=open("file.txt","r")
 for word in f.read().split():
     list.append(int(word))
 print("your entered data\n{}".format(list))
-
 list=sorted(list)
+
 my_list=LinkList()
 for data in list:
     my_list.Insert_Last(data)
