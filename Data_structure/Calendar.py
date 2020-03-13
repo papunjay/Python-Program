a148e006496919e28b99989e0239916cb88ac75d
papunjay <kumarpapunjay@gmail.com>
2020-03-13 12:49:39 +0530

    [papunjay]Add list of month

--- Data_structure/Calendar.py
+++ Data_structure/Calendar.py
@@ -1,2 +1,7 @@
 year=int(input())
 month=int(input())
+
+months=["January", "February", "March",
+            "April", "May", "June",
+            "July", "August", "September",
+            "October", "November", "December"]
