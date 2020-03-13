4c5ba70c698caf3d62e4cf2331dfa7882b16143b
papunjay <kumarpapunjay@gmail.com>
2020-03-13 15:36:06 +0530

    [papunjay].Add def check_anagram(prime_list):

--- prime_anagram.py
+++ prime_anagram.py
@@ -8,6 +8,17 @@ def prime_fun(Number_range):
         if(checker==1):
             prime_data.append(Number)
     return prime_data
-    
+def check_anagram(prime_list):
+    ana_list=[]
+    for ith in prime_list:
+        dist=0
+        for jth in prime_list:
+            if((ith!=jth )and (sorted(str(ith))==sorted(str(jth)))):
+                for index in range(len(ana_list)):
+                    if(ith==ana_list[index]):
+                        dist+=1
+                if(dist==0):
+                    ana_list.append(int(ith))
+    return ana_list    
 Number_range=1000
 prime_list=prime_fun(Number_range)
