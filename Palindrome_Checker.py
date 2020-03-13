f76bc3d42010998bf710c9b369214cf7463acfb7
papunjay <kumarpapunjay@gmail.com>
2020-03-13 15:20:54 +0530

    [papunjay].Add addRear

--- Palindrome_Checker.py
+++ Palindrome_Checker.py
@@ -3,5 +3,7 @@ class Deque:
         self.items=[]
     def isEmpty(self):
         return self.items==[]
+    def addRear(self,data):
+        self.items.insert(0,data)
 string=str(input("Enter the string..."))
 result=palChecker(string)
