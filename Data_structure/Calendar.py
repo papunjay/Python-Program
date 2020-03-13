94779e28f73ed61f7f2d4cff0c4d0fe347da9ff6
papunjay <kumarpapunjay@gmail.com>
2020-03-13 13:05:22 +0530

    [papunjay]Add print date of calendar

--- Data_structure/Calendar.py
+++ Data_structure/Calendar.py
@@ -15,20 +15,30 @@ def week_of_day(day,month,year):
     m0= month+12*((14-month)//12)-2
     d0=(day+x+31*m0//12) % 7
     return d0
+
 year=int(input())
 month=int(input())
-
 months=["January", "February", "March",
             "April", "May", "June",
             "July", "August", "September",
             "October", "November", "December"]
 days=[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
 result=isLeapyear(year)
 if(result==True):
     days[1]=29
-    
 print("{} {}".format(months[month-1],year))
 print("Su  Mo  Tu  We  Th  Fr  Sa")
 day=1
 count=0
 start_day=week_of_day(day,month,year)
+for space in range(start_day):
+    count+=1
+    print('  ',end=' ')
+for days in range(1,days[month+1]+1):
+    if(count<=7):
+        print('{0:<3}'.format(days), end=' ')
+        count+=1
+    else:
+        print("\n")
+        count=1   
