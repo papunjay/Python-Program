9f6e00e6d9a4667a00b1ab488ed836c56de2800a
papunjay <kumarpapunjay@gmail.com>
2020-03-13 12:52:25 +0530

    [papunjay]Add week_of_day

--- Data_structure/Calendar.py
+++ Data_structure/Calendar.py
@@ -9,6 +9,12 @@ def isLeapyear(year):
             return True
     else:
         return False
+def week_of_day(day,month,year):
+    y0=year-(14-month)//12
+    x=y0+y0//4 - y0//100 +y0//400
+    m0= month+12*((14-month)//12)-2
+    d0=(day+x+31*m0//12) % 7
+    return d0
 year=int(input())
 month=int(input())
 
