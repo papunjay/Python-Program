bcfe7f69cea951e0746677a740cb05bc27dab013
papunjay <kumarpapunjay@gmail.com>
2020-03-13 15:20:19 +0530

    [papunjay].Add isEmpty

--- Palindrome_Checker.py
+++ Palindrome_Checker.py
@@ -1,6 +1,7 @@
 class Deque:
     def __init__(self):
         self.items=[]
-
+    def isEmpty(self):
+        return self.items==[]
 string=str(input("Enter the string..."))
 result=palChecker(string)
