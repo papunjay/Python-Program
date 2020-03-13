c5027b3fe6b8a485b4b216556b123457f335d1e9
papunjay <kumarpapunjay@gmail.com>
2020-03-13 15:22:32 +0530

    [papunjay].Add removeRear

--- Palindrome_Checker.py
+++ Palindrome_Checker.py
@@ -7,5 +7,7 @@ class Deque:
         self.items.insert(0,data)
     def removeFront(self):
         return self.items.pop()
+    def removeRear(self):
+        return self.items.pop(0)
 string=str(input("Enter the string..."))
 result=palChecker(string)
