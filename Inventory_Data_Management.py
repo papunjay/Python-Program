4b5d2a1c7450ed52dbe91796816d2515abb8ba77
papunjay_kumar <kumarpapunjay@gmail.com>
2020-03-16 00:46:23 +0530

    [papunjay].Add calculate the all value

--- Inventory_Data_Management.py
+++ Inventory_Data_Management.py
@@ -2,5 +2,16 @@ import json
 file=open("F:\Bridgelabz\Python Developer\Inventory.json","r")
 data=json.load(file)
 file.close()
-
+print("-----Inventory Details-----")
+for value in data:
+    
+    price=weight=0
+    print("Name:=",value['name'])
+    price+=int(value["price per kg"])
+    weight+=int(value["weight"])
+    print("Weight:=",weight)
+    print("price:=",price)
+    total_price=price*weight
+    print("Total_value:=",total_price)
+    print("-----Inventory Details-------")
 
