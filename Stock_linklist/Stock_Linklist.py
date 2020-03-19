1188304ff8351e3cb80d3554020586a07e5bca48
papunjay_kumar <kumarpapunjay@gmail.com>
2020-03-20 02:03:27 +0530

    [papunjay].Add  class stack

--- Stock_linklist/Stock_Linklist.py
+++ Stock_linklist/Stock_Linklist.py
@@ -1,7 +1,13 @@
 class Node: 
-    
+
 	# Class to create nodes of linked list 
 	# constucter initializes node automatically 
 	def __init__(self,data): 
 		self.data = data 
 		self.next = None
+
+class Stack: 
+	
+	# head is default NULL 
+	def __init__(self): 
+		self.head = None
