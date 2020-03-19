2cdbfd6b31a0c01ac6a44cc0be502fe131f5a4bf
papunjay_kumar <kumarpapunjay@gmail.com>
2020-03-20 01:48:29 +0530

    [papunjay].Add modify the Inventory management

--- Inventory Management/Inventory_Data_Management.py
+++ Inventory Management/Inventory_Data_Management.py
@@ -1,16 +1,30 @@
 import json
-file=open("Inventory_Management.json","r")
-data=json.load(file)
-file.close()
-print("-----Inventory Details-----")
-for value in data:
+class inventoryManagement:
+    def __init__(self):
+        pass
+
+    #read the json file and return the json object 
+    def read_file(self):
+        file=open("Inventory_Management.json","r")
+        data=json.load(file)
+        file.close()
+        return data
     
-    price=weight=0
-    print("Name:=",value['name'])
-    price+=int(value["price per kg"])
-    weight+=int(value["weight"])
-    print("Weight:=",weight)
-    print("price:=",price)
-    total_price=price*weight
-    print("Total_value:=",total_price)
-    print("-----Inventory Details-------")
+    #calculate the total value for every data
+    def calculate(self,data):
+        print("-----Inventory Details-----")
+        for value in data:
+            price = weight=0
+            print("Name:=",value['name'])
+            price+=int(value["price per kg"])
+            weight+=int(value["weight"])
+            print("Weight:=",weight)
+            print("price:=",price)
+            total_price=price*weight
+            print("Total_value:=",total_price)
+            print("-----Inventory Details-------")
+
+if __name__ == '__main__' :
+   invM = inventoryManagement()
+   data=invM.read_file()
+   invM.calculate(data)
---
+++ Stock_linklist/Stock_Linklist.py
@@ -0,0 +1,7 @@
+class Node: 
+	
+	# Class to create nodes of linked list 
+	# constucter initializes node automatically 
+	def __init__(self,data): 
+		self.data = data 
+		self.next = None
